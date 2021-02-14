7d67dba9dec55337cb3445f14b696c73d2b874e3
Yahia E. Qumboz <yahia1997ezat@gmail.com>
2021-02-14 18:59:33 +0200

    init

---
+++ Data/Advertiser.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorApp.Data
+{
+    public class Advertiser
+    {
+        public Guid Id;
+        public string Name;
+        
+    }
+}
---
+++ Data/Media.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BlazorApp.Data
+{
+    public class Media
+    {
+        public string Name;
+        public DateTime CreatedAt;
+        public string MediaFile;
+        public Advertiser Advertiser;
+        
+    }
+}
